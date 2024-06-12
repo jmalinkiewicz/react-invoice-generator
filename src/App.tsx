a6d2e79051993c6cb23c939c38dde87eebc41da9
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2024-06-12 23:34:36 +0200

    add invoice details fields

--- src/App.tsx
+++ src/App.tsx
@@ -122,7 +122,7 @@ function App() {
               <h2 className="text-xl font-semibold">Invoice Details</h2>
               <div className="mt-4 flex gap-6">
                 <label className="w-full">
-                  Payment Due Date
+                  Date Issued
                   <input
                     placeholder="+48 123 456 789"
                     type="date"
@@ -130,13 +130,34 @@ function App() {
                   />
                 </label>
                 <label className="w-full">
-                  Bank Account
+                  Date Due
+                  <input placeholder="PO#" type="date" className="text-input" />
+                </label>
+              </div>
+              <div className="mt-4 flex gap-6">
+                <label className="w-full">
+                  Invoice Number
                   <input
-                    placeholder="customer@example.com"
+                    placeholder="INV-000001"
                     type="text"
                     className="text-input"
                   />
                 </label>
+                <label className="w-full">
+                  Reference #
+                  <input placeholder="PO#" type="text" className="text-input" />
+                </label>
+              </div>
+              <div className="mt-4 flex gap-6">
+                <label className="w-full">
+                  Currency
+                  <select className="text-input">
+                    <option className="option">USD</option>
+                    <option className="option">EUR</option>
+                    <option className="option">PLN</option>
+                  </select>
+                </label>
+                <div className="w-full"></div>
               </div>
             </div>
           </main>
